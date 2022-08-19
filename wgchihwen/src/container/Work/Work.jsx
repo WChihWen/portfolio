25b144bcfc1ed0ff760cd19ddc003d2ed1e4b92a
WChihWen <yaman0923@gmail.com>
2022-08-19 12:08:29 -0700

    update_work_section

--- wgchihwen/src/container/Work/Work.jsx
+++ wgchihwen/src/container/Work/Work.jsx
@@ -42,7 +42,7 @@ const Work = () => {
 		<h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
 		<div className="app__work-filter">
-			{['UI/UX', 'Web App', 'React JS', 'PHP', 'Case Study', 'All'].map((item, index) => (
+			{['Java', 'Python', 'React JS',  'Web App', 'PHP', 'System Design','Case Study', 'All'].map((item, index) => (
 			<div
 				key={index}
 				onClick={() => handleWorkFilter(item)}
